b33dff99422930b045a71175c1b090060481354c
Shane Mulligan <mullikine@gmail.com>
2021-07-07 23:59:50 +1200

    Wed 07 Jul 2021 23:59:50

--- src/lm_complete/core.clj
+++ src/lm_complete/core.clj
@@ -1,7 +1,20 @@
 (ns lm-complete.core
   (:gen-class))
 
+(use '[clojure.tools.cli :only [cli]])
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  ;; Process command-line arguments.
+
+  (cli args
+       ["-p" "--port" "Listen on this port" :parse-fn #(Integer. %)
+        :assoc-fn (fn [previous key val]
+                    (assoc previous key
+                           (if-let [oldval (get previous key)]
+                             (merge oldval val)
+                             (hash-set val))))]
+       ["-h" "--host" "The hostname" :default "localhost"]
+       ["-v" "--[no-]verbose" :default true]
+       ["-l" "--log-directory" :default "/some/path"]))
