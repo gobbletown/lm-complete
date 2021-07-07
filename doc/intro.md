d4e5e29debc4f119e74e3d1d48a2a8a1637a58a9
Shane Mulligan <mullikine@gmail.com>
2021-07-07 21:47:43 +1200

    07.07.21

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lm-complete
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/lm_complete/core.clj
@@ -0,0 +1,7 @@
+(ns lm-complete.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
